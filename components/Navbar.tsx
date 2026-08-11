8ee9dd40db5bcb31a6e5ee1bed9fd085d9a7b93e
nathaniel-knerr <knerrnathaniel@gmail.com>
2026-08-11 12:14:29 -0700

    Added icons for navbar buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,8 @@ import { supabase } from "@/lib/supabase";
 import type { User } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { MessageCirclePlus } from "lucide-react";
+import { HomeIcon } from "lucide-react";
 
 
 export default function Navbar() {
@@ -57,17 +59,20 @@ export default function Navbar() {
         <>
             <header className="flex flex-col items-center bg-gray-800 mb-4">
                 <nav className="w-[65%] flex flex-col md:flex-row justify-evenly items-center p-2">
-                    <Link href="/">
-                        <h1 className="text-2xl text-white font-bold m-1">Incognigram</h1>
-                    </Link>
+                    <h1 className="text-2xl text-white font-bold m-1">Incognigram</h1>
                     <div className="w-full flex flex-row justify-center md:justify-end">
                         {loading ?
                             <></>
                             :
                             <>
+                                <Link href="/" className="flex flex-col items-center justify-center text-sm text-white bg-purple-800 p-1 px-2 rounded-sm m-1">
+                                    <HomeIcon></HomeIcon>
+                                </Link>
                                 {user ? 
                                     <>
-                                        <Link href="/profile" className="flex flex-col items-center justify-center text-sm text-white bg-purple-800 p-1 px-2 rounded-sm m-1">Profile</Link>
+                                        <Link href="/profile" className="flex flex-col items-center justify-center text-sm text-white bg-purple-800 p-1 px-2 rounded-sm m-1">
+                                         <MessageCirclePlus></MessageCirclePlus>
+                                        </Link>
                                         <button onClick={logOut} className="flex flex-col items-center justify-center text-sm text-white bg-purple-800 p-1 px-2 rounded-sm m-1">Log Out</button>
                                     </>
                                     :
